2ef78600c10cca6a4253edc4721dfbe47c35323b
raifreire <rai.freire26@gmail.com>
2023-04-17 01:20:04 -0300

    add templates

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse('<h1> Teste </h1>')
+    return render(request, 'galeria/index.html')
