b6bf60dd58e31e3bc5676a8aec3affd3d062ddc7
raifreire <rai.freire26@gmail.com>
2023-04-24 17:39:50 -0300

    admin.py

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+#Configurando  Django Admin
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")# metodo list_display usado para definir o que sera exibido no painel do django admin web quando entrarmos no banco de dados
+
+admin.site.register(Fotografia, ListandoFotografias)#chamamos o metodo -> admin.site.register e passamos nosso Model e a função que contem as configurações de exibição
